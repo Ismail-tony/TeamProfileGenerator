7a98b81f48bd875e85bed7840d1e9e174bb9b257
Ismail-tony <ibabalola21@outlook.com>
2024-02-22 18:00:01 +0000

    added a manager and team members  prompt

--- index.js
+++ index.js
@@ -13,3 +13,46 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const teamMembers = [];
+
+const addManager = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the team manager's name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the team manager's id?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the team manager's email?",
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the team manager's office number?"
+        },
+    ])
+    
+    .then((answers) => {
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+        teamMembers.push(manager);
+        addTeamMember();
+    })
+}
+
+const addTeamMember = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'memberType',
+            message: 'What type of team member would you like to add?',
+            choices: ['Engineer', 'Intern', 'Finish building the team']
+        }
+    ])
+}
